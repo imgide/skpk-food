03f45a2185d456a70ee19773f98ba5089f9779c4
imgide <105896020+imgide@users.noreply.github.com>
2022-05-20 12:18:59 +0700

    first upload
    klo mau makan liat aja gpp

---
+++ database.js
@@ -0,0 +1,107 @@
+let menu =  [];
+
+let makanan = {
+foto:"https://cdn0-production-images-kly.akamaized.net/OHzX2xDRWYElycf6Uxz66HZQMPM=/2107x0:4231x2831/469x625/filters:quality(75):strip_icc():format(webp)/kly-media-production/medias/2825744/original/089180200_1560239608-shutterstock_1178128243.jpg",
+harga: 15000,
+kode: "BRG001",
+nama: "mie",
+rating: "5,0",
+stock: 10,
+
+};
+
+menu.push (makanan)
+makanan = {
+    foto: "https://cdn0-production-images-kly.akamaized.net/aDZbOOV2WeJCgCiBFgFQwoXJN64=/640x360/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/2029093/original/066661300_1521949447-Bakso.jpg",
+    harga: 15000,
+    kode: "BRG001",
+    nama: "bakso",
+    rating: "5,0",
+    stock: 20,
+
+};
+
+menu.push (makanan)
+makanan = {
+    foto: "https://www.madiunpos.com/files/2020/03/HeciInstagram.kreasimasakanrumah.jpg",
+    harga: 2000,
+    kode: "BRG001",
+    nama: "Heci",
+    rating: "5,0",
+    stock: 100,
+
+};
+
+menu.push (makanan)
+makanan = {
+    foto: "https://blogunik.com/wp-content/uploads/2021/09/Permen-Terenak-Dan-Terbaik-Di-Dunia.jpg",
+    harga: 500,
+    kode: "BRG001",
+    nama: "permen",
+    rating: "5,0",
+    stock: 1000,
+};
+
+menu.push (makanan)
+let minuman = {
+    foto: "https://res.cloudinary.com/dk0z4ums3/image/upload/v1589962541/attached_image/cara-memilih-air-kemasan-yang-baik.jpg",
+    harga: 6000,
+    kode: "BRG001",
+    nama: "air mineral",
+    rating: "5,0",
+    stock: 50,
+};    
+
+menu.push (makanan);
+makanan = {
+    foto: "https://assets-pergikuliner.com/uploads/image/picture/146462/picture-1452742035.JPG",
+    harga: 26000,
+    kode: "BRG001",
+    nama: "ichiraku ramen",
+    rating: "5,0",
+    stock: 80,
+};
+
+menu.push (minuman);
+minuman = {
+    foto: "https://media.istockphoto.com/photos/pour-soft-drink-in-glass-with-ice-splash-on-dark-background-picture-id1062831310?k=20&m=1062831310&s=612x612&w=0&h=kvadNOzqCnDYUH83ffviQie1G7E62kBzvZZ8CmZuoxU=",
+    harga: 5000,
+    kode: "BRG001",
+    nama: "coca-caco",
+    rating: "5,0",
+    stock: 100,
+};
+
+
+menu.push (makanan);
+makanan = {
+    foto: "https://www.piknikdong.com/wp-content/uploads/2021/11/Resep-Ayam-Geprek-Bensu-min.jpg",
+    harga: 10000,
+    kode: "BRG001",
+    nama: "ayamgeprek",
+    rating: "5,0",
+    stock: 44,
+};
+
+menu.push (minuman);
+minuman = {
+    foto: "https://cdn0-production-images-kly.akamaized.net/ExQxbtzaalV8WkA_Oldv05Yn0NI=/640x360/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/3985064/original/011650300_1649125907-20211102101943-1-8-cara-membuat-es-buah-yang-segar-dan-enak-009-fatimah-rahmawati.jpg",
+    harga: 18000,
+    kode: "BRG001",
+    nama: "esbuah",
+    rating: "5,0",
+    stock: 50,
+};
+
+menu.push (makanan);
+makanan = {
+    foto: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Sate_Ponorogo.jpg/640px-Sate_Ponorogo.jpg",
+    harga: 15000,
+    kode: "BRG001",
+    nama: "sate",
+    rating: "5,0",
+    stock: 20,
+};
+
+menu.push (makanan)
+console.log (menu)
